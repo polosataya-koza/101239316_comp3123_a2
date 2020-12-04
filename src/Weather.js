904653cffa609786f7f876d52dcfec4108aa99c9
Evgeniya <polosataya.koza@gmail.com>
2020-12-04 18:57:34 -0500

    Change main page

--- src/Weather.js
+++ src/Weather.js
@@ -9,7 +9,7 @@ const Weather = () => {
     var date = new Date();
 
     const fetchWeather = async () => {
-        const { data } = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=6508c02ccc05f225734d2568a4c6dadd');     
+        const { data } = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=6508c02ccc05f225734d2568a4c6dadd');
         console.log(data);
         setWeather(data);
         return data;
@@ -17,11 +17,14 @@ const Weather = () => {
 
     return (
         <div className="main-container">
-            <button type="button" className="button-click" onClick={fetchWeather}>Tap to see new info about the weather in Toronto</button><br/>
+            <div className="topic">
+            <h1>Weather in Toronto</h1>
+            <button type="button" className="button-click" onClick={fetchWeather}>Upload</button><br/>
+            </div>
             {weather.main && (
                 <div className="city">
                     <h2 className="city-name"><span>{weather.name}, {weather.sys.country}</span></h2>
-                    <h3 className="city-name-med">{months[(date.getMonth()).toLocaleString()]} {date.getDate()}, {days[date.getDate()]}</h3>
+                    <h3 className="city-name-med">{months[(date.getMonth()).toLocaleString()]} {date.getDate()}, {days[date.getDate()]+1}</h3>
                     <div className="city-temp">{Math.round(weather.main.temp)}<sup>&deg;C</sup></div>
                     <div className="city-temp-medf">Feels like: {Math.round(weather.main.feels_like)}<sup>&deg;C</sup></div>
                     <div className="city-temp-mini">min: {Math.round(weather.main.temp_min)}<sup>&deg;C</sup> max: {Math.round(weather.main.temp_max)}<sup>&deg;C</sup></div>
